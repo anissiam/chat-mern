ba90c83de1336d240f10617b42f7a409fba4e1af
anis <anis.siam@hotmail.com>
2025-09-07 14:54:56 +0300

    fix bugs

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
 app.use(cors({
-    origin: [process.env.ORIGIN],
+    origin: '*',
     methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
     credentials:true,
     optionSuccessStatus:200,
